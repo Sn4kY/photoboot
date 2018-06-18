afc198e6bb4250dc426e72f5bd174f5b2d7276b8
Sn4kY <sn4ky@sn4ky.net>
2018-06-18 22:39:46 +0200

    correcting picture size when resize

--- process_image.sh
+++ process_image.sh
@@ -2,12 +2,12 @@
 DATE=$(date +%Y-%m-%d-%H-%M-%S)
 
 sudo killall fbi
-sudo fbi -a --noverbose -T 1 pictures/shot.jpg
+sudo fbi -a --noverbose -T 1 /tmp/shot.jpg
 sleep 1
 #convert -resize 125x147 -gravity center /home/pi/photobooth_images/shot.jpg /home/pi/temp/photobooth-${DATE}.jpg
 # Adding dpi to 300 to fit printer characteristics
 #convert -density 300 -resize 520x612 -gravity center /home/pi/photobooth_images/shot.jpg /home/pi/temp/photobooth-${DATE}.jpg
 # Adding flip image vertically
-convert -density 300 -resize 520x612 -gravity center -flop pictures/shot.jpg /tmp/photobooth-${DATE}.jpg
-mv pictures/shot.jpg archives/shot_${DATE}.jpg
+convert -density 300 -resize 520x602 -gravity center -flop /tmp/shot.jpg /tmp/photobooth-${DATE}.jpg
+mv /tmp/shot.jpg archives/shot_${DATE}.jpg
 sudo killall fbi
