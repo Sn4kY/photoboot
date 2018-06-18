56da623134a3b6a8e5b79314c14a2c2299403399
Sn4kY <sn4ky@sn4ky.net>
2018-06-18 22:40:54 +0200

    correcting margins for printing with CP1200 with option in CUPS 'Borderless = NO'

--- assemble.sh
+++ assemble.sh
@@ -5,7 +5,9 @@ sudo killall fbi
 montage /tmp/photobooth-*.jpg -tile 2x2 -border 5 -geometry +10+10 -density 300 -units PixelsPerInch /tmp/montage2.jpg
 sudo  fbi -a --noverbose -T 1 /tmp/montage2.jpg
 montage /tmp/montage2.jpg res/photobooth_label.jpg -tile 1x2 -geometry +5+5 -density 300 -units PixelsPerInch /tmp/montage3.jpg
-montage /tmp/montage3.jpg -border 35 -bordercolor "#ffffff" -density 300 -units PixelsPerInch -geometry +0+0 /tmp/montage4.jpg
+#montage /tmp/montage3.jpg -border 15 -bordercolor "#ffffff" -density 300 -units PixelsPerInch -geometry +0+0 -gravity north /tmp/montage4.jpg
+convert /tmp/montage3.jpg -bordercolor "#ffffff" -splice 0x15 -density 300 -units PixelsPerInch -geometry +0+0 -gravity north /tmp/montage4.jpg
+
 while [ true ]
 do
 	if [ $(/usr/sbin/lpinfo --include-schemes usb -v | grep -c "Canon/SELPHY%20CP1200") -eq 1 ]
@@ -15,7 +17,7 @@ do
 	sleep 2
 done
 lp -d Canon_SELPHY_CP1200 -P 1 /tmp/montage4.jpg
-mv -f /tmp/montage4.jpg archives/pb_${DATE}.jpg
+mv -f /tmp/montage3.jpg archives/pb_${DATE}.jpg
 rm -vf /tmp/*.jpg
 sleep 70
 sudo killall fbi
